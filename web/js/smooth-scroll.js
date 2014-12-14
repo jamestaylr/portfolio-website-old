db9972ddb37a21a8f884c316b9f26fb052a72667
James Taylor <jamestaylr@gmail.com>
2014-12-13 22:35:53 -0500

    Changed smooth scroll padding offset

--- web/js/smooth-scroll.js
+++ web/js/smooth-scroll.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 				target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
 				if (target.length) {
 					$('html,body').animate({
-						scrollTop: target.offset().top - 35
+						scrollTop: target.offset().top - 50
 					}, 500);
 					return false;
 				}
