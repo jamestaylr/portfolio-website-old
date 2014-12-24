b9df2eeca70a0c3f156ef2d76cda52fad4de0ada
James Taylor <jamestaylr@gmail.com>
2014-12-24 00:30:26 -0500

    Included server framework for website deployment

---
+++ server.py
@@ -0,0 +1,38 @@
+import tornado.httpserver
+import tornado.websocket
+import tornado.ioloop
+import tornado.web
+import os
+ 
+class IndexHandler(tornado.web.RequestHandler):
+    """ Creates the web server
+    """
+    @tornado.web.asynchronous
+    def get(self):
+        self.render('web/index.html')
+
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r'/', IndexHandler),
+        ]
+        settings = {
+            "debug": True,
+            "static_path": os.path.join(os.path.dirname(__file__), "web")
+        }
+        tornado.web.Application.__init__(self, handlers, **settings)
+
+
+application = Application()
+""" Defines the server parameters
+"""
+
+
+print ("Starting server.")
+http_server = tornado.httpserver.HTTPServer(application)
+http_server.listen(8888)
+
+# starts the main IO loop
+main_loop = tornado.ioloop.IOLoop.instance()
+main_loop.start()
